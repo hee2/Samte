46d41629a0a6689ae413f9a7a516d0afe3723485
heechoi1 <heechoi1@gmail.com>
2022-05-23 21:50:01 -0400

    0523 changes

--- Step12/src/main/java/com/in28minutes/springboot/web/service/LoginService.java
+++ Step12/src/main/java/com/in28minutes/springboot/web/service/LoginService.java
@@ -8,6 +8,9 @@ public class LoginService {
 
 	public boolean validateUser(String userid, String password) {
 		// in28minutes, dummy
+		System.out.println("---------2");
+		System.out.println("userid: " + userid);
+		System.out.println("password: " + password);
 		return userid.equalsIgnoreCase("hee")
 				&& password.equalsIgnoreCase("pwd");
 	}
